258d4244f6f8c8a49480ec7a41b11d241b6ece01
DennisJ1 <gusjande@student.gu.se>
2020-12-10 11:49:36 +0100

    Added reusable colors

---
+++ lib/assets/CustomColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const customPink = const Color(0xfff3d9d8);
+const nyanza = const Color(0xffd8f3dc);
+const turquoiseGreen = const Color(0xff95d5b2);
+const oceanGreen = const Color(0xff74c69d);
+const illuminatingEmerald = const Color(0xff40916c);
+const bottleGreen = const Color(0xff2d6a4f);
+const brunsWickGreen = const Color(0xff1b4332);
+const darkJungleGreen = const Color(0xff081c15);

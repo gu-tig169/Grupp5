689b95060e17ce9e9ad8131577e3ed5fc74a6017
DennisJ1 <gusjande@student.gu.se>
2020-12-11 14:06:17 +0100

    Created a select profile view. Added another pink color and removed colors.dart

--- lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class ColorClass {
-  Color pinkcolor = const Color(0xfff3d9d8);
-}
---
+++ lib/view/selectProfileView.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:triviaholic/assets/CustomColors.dart';
+import 'package:triviaholic/view/widgets/navbar.dart';
+
+class SelectProfileView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   backgroundColor: illuminatingEmerald,
+      //   title: Text('Select Profile'),
+      // ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment(0.0, -1.0),
+                  end: Alignment(0.0, 0.7),
+                  colors: [turquoiseGreen, nyanza])),
+          child: Column(
+            children: [
+              spaceBetween(130),
+              selectProfileText(),
+              profileDropDown(),
+              spaceBetween(40),
+              newProfileButton(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+}
+
+Widget selectProfileText() {
+  return Container(
+      child: Text(
+    'Select Profile',
+    style: TextStyle(
+        fontSize: 36, fontWeight: FontWeight.w300, color: darkJungleGreen),
+  ));
+}
+
+Widget profileDropDown() {
+  return Container(
+    margin: EdgeInsets.only(top: 20, left: 40, right: 40),
+    decoration: BoxDecoration(
+        border: Border.all(width: 1.5, color: brunsWickGreen),
+        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+    child: DropdownButton<String>(
+        dropdownColor: nyanza,
+        isExpanded: true,
+        iconSize: 50,
+        icon: Icon(
+          Icons.arrow_drop_down,
+          color: brunsWickGreen,
+        ),
+        iconEnabledColor: darkJungleGreen,
+        items: <String>['Dennis', 'Alban', 'Lukas', 'Niko', 'Noa']
+            .map((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value, style: TextStyle(fontSize: 22)),
+          );
+        }).toList(),
+        onChanged: (_) {}),
+  );
+}
+
+Widget newProfileButton() {
+  return Container(
+    margin: EdgeInsets.only(bottom: 50, top: 130),
+    alignment: Alignment.bottomCenter,
+    child: SizedBox(
+      width: 250,
+      height: 70,
+      child: RaisedButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: BorderSide(color: customPink)),
+          color: customPink,
+          child: Text(
+            'New Profile',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+          ),
+          onPressed: () {}),
+    ),
+  );
+}
+
+Widget spaceBetween(double height) {
+  return SizedBox(height: height);
+}

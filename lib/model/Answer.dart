da05fdcfb7e1595e9d66b9286b7b661a4251809b
DennisJ1 <gusjande@student.gu.se>
2020-12-18 14:38:34 +0100

    Started working on the quiz API

--- lib/model/Answer.dart
+++ lib/model/Answer.dart
@@ -1,4 +1,6 @@
 class Answer {
   String _title;
   bool _correctAnswer;
+
+  Answer(this._title, this._correctAnswer);
 }
--- lib/model/Question.dart
+++ lib/model/Question.dart
@@ -1,6 +1,16 @@
 import 'package:triviaholic/model/Answer.dart';
 
 class Question {
-  String _questions;
+  String _question;
   List<Answer> _answers;
+
+  Question(this._question, this._answers);
+
+  String get question {
+    return _question;
+  }
+
+  List<Answer> get answers {
+    return _answers;
+  }
 }
